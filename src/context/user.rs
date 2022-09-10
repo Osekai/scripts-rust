f917dbc172d021ddf45add7a22cb4bb912661450
MaxOhn <ohn.m@hotmail.de>
2022-09-10 19:18:03 +0200

    request pages concurrently across modes

--- src/context/user.rs
+++ src/context/user.rs
@@ -1,7 +1,10 @@
 use std::collections::{BinaryHeap, HashSet};
 
 use eyre::{Context as _, Report, Result};
-use rosu_v2::prelude::{GameMode, OsuError};
+use rosu_v2::{
+    prelude::{GameMode, OsuError, Rankings},
+    OsuResult,
+};
 
 use crate::{
     model::{SlimBadge, UserFull},
@@ -33,49 +36,54 @@ impl Context {
         user_ids: &mut HashSet<u32, IntHasher>,
         max_page: u32,
     ) {
-        info!("Requesting the first {max_page} leaderboard pages for all modes...");
-
-        let modes = [
-            GameMode::Osu,
-            GameMode::Taiko,
-            GameMode::Catch,
-            GameMode::Mania,
-        ];
-
         let max_page = max_page.min(200);
         user_ids.reserve(4 * 40 * max_page as usize);
         let mut eta = Eta::default();
 
-        for (mode, i) in modes.into_iter().zip(0..) {
-            for page in 1..=max_page {
-                let page_fut = self.osu.performance_rankings(mode).page(page);
+        info!("Requesting the first {max_page} leaderboard pages for all modes...");
 
-                let rankings = match page_fut.await {
-                    Ok(rankings) => rankings,
+        for page in 1..=max_page {
+            let std_fut = self.osu.performance_rankings(GameMode::Osu).page(page);
+            let tko_fut = self.osu.performance_rankings(GameMode::Taiko).page(page);
+            let ctb_fut = self.osu.performance_rankings(GameMode::Catch).page(page);
+            let mna_fut = self.osu.performance_rankings(GameMode::Mania).page(page);
+
+            let (std_res, tko_res, ctb_res, mna_res) =
+                tokio::join!(std_fut, tko_fut, ctb_fut, mna_fut);
+
+            fn extend_users(
+                rankings_res: OsuResult<Rankings>,
+                user_ids: &mut HashSet<u32, IntHasher>,
+                page: u32,
+                mode: GameMode,
+            ) {
+                match rankings_res {
+                    Ok(rankings) => {
+                        user_ids.extend(rankings.ranking.into_iter().map(|user| user.user_id))
+                    }
                     Err(err) => {
                         let wrap =
                             format!("Failed to retrieve leaderboard page {page} for {mode:?}");
                         error!("{:?}", Report::from(err).wrap_err(wrap));
-
-                        continue;
                     }
-                };
-
-                user_ids.extend(rankings.ranking.into_iter().map(|user| user.user_id));
-                eta.tick();
-
-                if page % 50 == 0 {
-                    let curr = i * 200 + page;
-                    let max = max_page * 4;
-
-                    info!(
-                        "Leaderboard progress: {curr}/{max} | ETA: {}",
-                        eta.estimate((max - curr) as usize),
-                    );
                 }
             }
 
-            info!("Finished requesting {max_page} leaderboard pages for {mode:?}");
+            extend_users(std_res, user_ids, page, GameMode::Osu);
+            extend_users(tko_res, user_ids, page, GameMode::Taiko);
+            extend_users(ctb_res, user_ids, page, GameMode::Catch);
+            extend_users(mna_res, user_ids, page, GameMode::Mania);
+
+            eta.tick();
+
+            if page % 25 == 0 {
+                info!(
+                    "Leaderboard progress: {page}/{max_page} | ETA: {}",
+                    eta.estimate((max_page - page) as usize),
+                );
+            }
+
+            info!("Finished requesting {max_page} leaderboard pages for all modes");
         }
     }
 
